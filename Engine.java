b13422b508b20a3b39e1d295758e16f8f4876adb
Joodongri <33230149+Joodongri@users.noreply.github.com>
2017-11-13 19:11:38 -0800

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -142,6 +142,14 @@ public class Engine implements Serializable
 		return true;
 	}
 	
+	
+	// modified by Dongri Zhu
+	public boolean checkBullet() {							//check the bullet before shoot in the UI
+		if(grid.getPlayer().hasBullet())
+			return true;
+		return false;
+	}
+	
 	/**
 	 * The hub shoot method, a method that accepts a String value as a parameter. Depending on the value of this
 	 * String, the player will be allowed to shoot in a certain direction. The corresponding direction calls a
@@ -149,47 +157,128 @@ public class Engine implements Serializable
 	 */
 	public boolean shoot(String input)
 	{
-		return shootUp();
+		grid.getPlayer().shoot();
+		
+		if(input=="w" || input=="W")
+			return shootUp();
+		
+		else if(input=="s" || input=="S")
+			return shootDown();
+		
+		else if(input=="d" || input=="D")
+			return shootRight();
+		
+		else
+			return shootLeft();
 	}
 	
 	/**
 	 * A method that determines if a player is able to shoot upwards, according to specific shooting rules.
 	 */
 	public boolean shootUp()
-	{
-		return true;
+	{	
+		int row = grid.getPlayer().getRow() -1;						//start from the above square of the spy
+		int col = grid.getPlayer().getColumn();
+				
+		while(row >= 0) {
+			
+			if(grid.getGridObject(row,col).isARoom())				//block by the room
+				return false;
+			
+			if(grid.getGridObject(row,col).isAnEnemy()) {			//kill the first enemy in the line
+				enemyIsKilled(row, col);
+				return true;
+			}
+			
+			row--;
+		}
+		
+		return false;
+		
+		// For the false, display "Nothing happened" in the UI
+		// For the true, display "enemy is killed"
 	}
-	
+
 	/**
 	 * A method that determines if a player is able to shoot downwards, according to specific shooting rules.
 	 */
 	public boolean shootDown()
 	{
-		return true;
+		int row = grid.getPlayer().getRow() +1;						//start from the bottom square of the spy
+		int col = grid.getPlayer().getColumn();
+				
+		while(row <= 8) {
+			
+			if(grid.getGridObject(row,col).isARoom())				//block by the room
+				return false;
+			
+			if(grid.getGridObject(row,col).isAnEnemy()) {			//kill the first enemy in the line
+				enemyIsKilled(row, col);
+				return true;
+			}
+			
+			row++;
+		}
+		
+		return false;
 	}
 	
-	/**
-	 * A method that determines if a player is able to shoot leftwards, according to specific shooting rules.
-	 */
-	public boolean shootLeft()
-	{
-		return true;
-	}
 	
 	/**
 	 * A method that determines if a player is able to shoot rightwards, according to specific shooting rules.
 	 */
 	public boolean shootRight()
 	{
-		if(0 == 0 && 4 >= 2)
-		{
-			return true;
+		int row = grid.getPlayer().getRow();						
+		int col = grid.getPlayer().getColumn() +1;					//start from the right square of the spy
+				
+		while(col <= 8) {
+			
+			if(grid.getGridObject(row,col).isARoom())				//block by the room
+				return false;
+			
+			if(grid.getGridObject(row,col).isAnEnemy()) {			//kill the first enemy in the line
+				enemyIsKilled(row, col);
+				return true;
+			}
+			
+			col++;
 		}
-		else
-		{
-			return false;
-		}
+		
+		return false;
 	}
+	
+	
+	/**
+	 * A method that determines if a player is able to shoot leftwards, according to specific shooting rules.
+	 */
+	public boolean shootLeft()
+	{	
+		int row = grid.getPlayer().getRow();						
+		int col = grid.getPlayer().getColumn() -1;					//start from the left square of the spy
+				
+		while(col >= 0) {
+			
+			if(grid.getGridObject(row,col).isARoom())				//block by the room
+				return false;
+			
+			if(grid.getGridObject(row,col).isAnEnemy()) {			//kill the first enemy in the line
+				enemyIsKilled(row, col);
+				return true;
+			}
+			
+			col--;
+		}
+		
+		return false;
+	}
+	
+	private void enemyIsKilled(int row, int col)								// modified by Dongri
+	{
+		//here needs a duplicate case, which the enemy duplicates with other items
+		grid.removeGridObject(row, col);
+	}
+
 	
 	/**
 	 * This method implements the "look" action that can be performed by the player. It will properly
