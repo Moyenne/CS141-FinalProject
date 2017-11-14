98c970eb04c13ca14edb9b16f55aba7314769479
Moyenne <suedoo62@gmail.com>
2017-11-13 22:21:51 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main
 	 */
 	public static void main(String[] args)
 	{
-		//UI ui = new UI(new Engine());
-		//ui.startGame();
+		UI ui = new UI(new Engine());
+		ui.startGame();
 	}
 }
