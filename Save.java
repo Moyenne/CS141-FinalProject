95180a0beab819e9053e8cd9ded88c5a056aa3d1
Clovenski <32505043+Clovenski@users.noreply.github.com>
2017-11-20 17:19:57 -0800

    Update Save.java
    -still needs proper documentation to clarify the static methods' functionalities

--- Save.java
+++ Save.java
@@ -1,5 +1,13 @@
 package edu.cpp.cs.cs141.FinalProject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * This class represents the file saving and loading system for the game,
  * allowing the player to save their progress/position, and return to it
@@ -12,17 +20,36 @@ public class Save
 	 * A method to save a file, which saves the location and identity of all
 	 * GridObjects on the grid.
 	 */
-	public void save()
-	{
-		
+	public static void WriteToFile(Grid grid, String saveFileName){
+		try {
+			FileOutputStream f = new FileOutputStream(new File(".\\savefiles\\" + saveFileName));
+			ObjectOutputStream o = new ObjectOutputStream(f);
+			
+		o.writeObject(grid);
+		o.close();
+		f.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Error initializing stream");
+		}	
 	}
 	
 	/**
 	 * A method to load a file, which replaces all GridObjects that were on the
 	 * grid back to their correct positions, with their correct internal values.
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
 	 */
-	public void load()
+	public static Grid readFromFile(String nameOfFile) throws IOException, ClassNotFoundException
 	{
+		FileInputStream fi = new FileInputStream(new File(".\\savefiles\\" + nameOfFile));
+		ObjectInputStream oi = new ObjectInputStream(fi);
 		
+		Grid loadedGrid = (Grid)oi.readObject();
+		fi.close();
+		oi.close();
+		return loadedGrid;
 	}
 }
+
